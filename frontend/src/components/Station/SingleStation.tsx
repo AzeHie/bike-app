aff5f5c3e79d95d45bb47c88903f99fe796d3365
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-04-27 12:02:07 +0300

    Frontend: Working with single station view. Added average distances etc

--- frontend/src/components/Station/SingleStation.tsx
+++ frontend/src/components/Station/SingleStation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Station from "../../shared/models/Station";
@@ -6,17 +6,38 @@ import bikeimg from "../../assets/images/citybiking.jpg";
 import Map from "../../shared/layout/Map";
 import "./SingleStation.css";
 import Modal from "../../shared/layout/Modal";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/layout/ErrorModal";
+import LoadingSpinner from "../../shared/layout/LoadingSpinner";
 
 const SingleStation: React.FC<{
   station: Station;
-  startedJourneys: number;
-  endedJourneys: number;
 }> = (props) => {
   const navigate = useNavigate();
   const [showMap, setShowMap] = useState<boolean>(false);
+  const [startedJourneys, setStartedJourneys] = useState<number>();
+  const [endedJourneys, setEndedJourneys] = useState<number>();
+  const [startedAvg, setStartedAvg] = useState<number>();
+  const [endedAvg, setEndedAvg] = useState<number>();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const openMapHandler = () => setShowMap(true);
   const closeMapHandler = () => setShowMap(false);
+
+  useEffect(() => {
+    const fetchJourneyData = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/stations/journeydata/${props.station.stationId}`
+        );
+        setStartedJourneys(responseData.startedJourneys);
+        setEndedJourneys(responseData.endedJourneys);
+        setStartedAvg(responseData.startedAvg);
+        setEndedAvg(responseData.endedAvg);
+      } catch (err) {}
+    };
+    fetchJourneyData();
+  }, [sendRequest, props.station.stationId]);
 
   const backToListHandler = () => {
     navigate("/stations");
@@ -24,7 +45,12 @@ const SingleStation: React.FC<{
 
   return (
     <React.Fragment>
-      <Modal show={showMap} onCancel={closeMapHandler} header={props.station.stationAddress}>
+      <ErrorModal error={error} onClear={clearError} />
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.station.stationAddress}
+      >
         <div className="map-container">
           <Map y={props.station.y} x={props.station.x} />
         </div>
@@ -38,11 +64,51 @@ const SingleStation: React.FC<{
         </div>
         <img className="stationImg" src={bikeimg} alt="single station" />
         <div className="journeys-total">
-          <p>Journeys started from the station: {props.startedJourneys}</p>
-          <p>Journeys ended on the station: {props.endedJourneys}</p>
+          <div className="started-journeys">
+            <div className="journeys-total-item">
+              <p>Journeys started from the station:</p>
+              {isLoading && <p className="loading">Loading..</p>}
+              {!isLoading && (
+                <p>
+                  <b>{startedJourneys}</b>
+                </p>
+              )}
+            </div>
+            <div className="journeys-total-item">
+              <p>Average distance of started journeys:</p>
+              {isLoading && <p className="loading">Loading..</p>}
+              {!isLoading && startedAvg && (
+                <p>
+                  <b>{startedAvg}km</b>
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="ended-journeys">
+            <div className="journeys-total-item">
+              <p>Journeys ended on the station: </p>
+              {isLoading && <p className="loading">Loading..</p>}
+              {!isLoading && (
+                <p>
+                  <b>{endedJourneys}</b>
+                </p>
+              )}
+            </div>
+            <div className="journeys-total-item">
+              <p>Average distance of started journeys:</p>
+              {isLoading && <p className="loading">Loading..</p>}
+              {!isLoading && endedAvg && (
+                <p>
+                  <b>{endedAvg}km</b>
+                </p>
+              )}
+            </div>
+          </div>
         </div>
         <div>
-          <button className="generic-button" onClick={openMapHandler}>SHOW ON MAP</button>
+          <button className="generic-button" onClick={openMapHandler}>
+            SHOW ON MAP
+          </button>
           <button className="generic-button" onClick={backToListHandler}>
             BACK TO STATION LIST
           </button>
--- frontend/src/pages/Station/StationDetail.tsx
+++ frontend/src/pages/Station/StationDetail.tsx
@@ -11,8 +11,6 @@ import Card from "../../shared/layout/Card";
 const StationDetail: React.FC = () => {
   const stationId = useParams().stationId;
   const [loadedStation, setLoadedStation] = useState<Station>();
-  const [startedJourneys, setStartedJourneys] = useState<number>(0);
-  const [endedJourneys, setEndedJourneys] = useState<number>(0);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   useEffect(() => {
@@ -22,19 +20,9 @@ const StationDetail: React.FC = () => {
           `http://localhost:5000/api/stations/${stationId}`
         );
         setLoadedStation(responseData.station);
-        setStartedJourneys(responseData.startedJourneys);
-        setEndedJourneys(responseData.endedJourneys);
       } catch (err) {}
     };
     fetchStation();
-
-    // const fetchJourneyData = async () => {
-    //   try {
-    //     const responseData = await sendRequest(
-    //       `http://localhost:5000/api/journeys/$`
-    //     )
-    //   }
-    // }
   }, [sendRequest, stationId]);
 
   return (
@@ -50,8 +38,6 @@ const StationDetail: React.FC = () => {
           {!isLoading && loadedStation && (
             <SingleStation
               station={loadedStation}
-              startedJourneys={startedJourneys}
-              endedJourneys={endedJourneys}
             />
           )}
         </Card>
